fee1c68cf9818c0ab521508c7a67b9c47648fa1d
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-22 22:00:33 -0700

    enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 autoreconf --force --verbose --install
 
+export CXX_FOR_BUILD=${CXX}
 ./configure --prefix=$PREFIX --disable-ecore --disable-tests --disable-examples || { cat config.log ; exit 1 ; }
 make
 make install
